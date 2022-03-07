a3855323d10a24dbb7ea5bb3b849eae46c9d34b7
Connor Gray <accounts@connorgray.com>
2022-03-06 22:11:46 -0600

    cleanup: Move current main.rs example diagram to dedicated examples/two_boxes.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use std::path::Path;
-
-use diagram_maker::{
-    diagram::{Box, Label, Text},
-    Diagram,
-};
-
 fn main() {
-    let diagram = Diagram {
-        boxes: vec![
-            Box {
-                label: Label("Box A".into()),
-                text: Text("This is Box A".into()),
-            },
-            Box {
-                label: Label("Box B".into()),
-                text: Text("This is Box B".into()),
-            },
-        ],
-        arrows: vec![],
-    };
-
-    diagram.save_to_png(Path::new("./diagram.png")).unwrap()
+    todo!()
 }
