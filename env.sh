da449bc57d2b252c1f255e65be1f334eb7bbdd03
Connor Gray <code@connorgray.com>
2024-09-07 11:55:36 -0500

    docs: Explain purpose of env vars in env.sh

--- env.sh
+++ env.sh
@@ -4,8 +4,15 @@
 
 LLVM_18="/opt/homebrew/Cellar/llvm/18.1.8"
 
-export MLIR_SYS_180_PREFIX="$LLVM_18"
-export LLVM_SYS_180_PREFIX="$LLVM_18"
+export MLIR_SYS_180_PREFIX="$LLVM_18"    # Needed to build `melior`
+export LLVM_SYS_180_PREFIX="$LLVM_18"    # Needed to build `inkwell`
 
 # Ensure llvm-config from LLVM 18 is visible on the path.
 export PATH="$LLVM_18/bin/:$PATH"
+
+# Note:
+#   Might be useful in the future, I had to set it at one point but don't
+#   remember why:
+#
+#   $ echo $SDKROOT
+#   /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
